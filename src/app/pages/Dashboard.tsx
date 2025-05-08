3af7a130a4e2412a43a345ef171fe425e5178eb1
Arielito Manorina <arielitorobles1525@gmail.com>
2025-05-08 13:02:25 +0800

    Add suspense for useSearchParams() hook

--- src/app/pages/Dashboard.tsx
+++ src/app/pages/Dashboard.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import Content from "../components/Content";
 import NavBar from "../components/NavBar";
 
@@ -5,7 +6,9 @@ export default function Dashboard() {
   return (
     <div className="font-montserrat flex h-full w-full flex-col items-center overflow-hidden rounded-xl bg-gradient-to-bl from-[#ffe4e6] to-[#ccfbf1] text-[#735557] lg:w-[90%] xl:w-[80%]">
       <NavBar />
-      <Content />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Content />
+      </Suspense>
     </div>
   );
 }
